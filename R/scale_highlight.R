24a52920a9b6ca8bde80a38a9e4a56624ed6393d
Hiroaki Yutani <yutani.ini@gmail.com>
2017-05-28 00:52:08 +0900

    add files

---
+++ R/scale_highlight.R
@@ -0,0 +1,51 @@
+#' Highlight With Predicates
+#'
+#' @export
+scale_highlight_colour <- function(.predicate, ..., type = "seq", palette = 1, direction = 1) {
+  highlight_scale("colour", "brewer", scales::brewer_pal(type, palette, direction), predicate = rlang::enquo(.predicate), ...)
+}
+
+#' ggproto for Highlight
+#'
+#' @name ScaleHighlight
+#' @format NULL
+#' @usage NULL
+#' @export
+ScaleHighlight <- ggplot2::ggproto("Scale", ggplot2::ScaleDiscrete,
+  predicate = NULL,
+  map_df = function(self, df, i = NULL) {
+    ggplot2::ggproto_parent(ggplot2::ScaleDiscrete, self)$map_df(df, i)
+  }
+)
+
+# TODO: do not copy and paste codes
+highlight_scale <- function(aesthetics, scale_name, palette, name = ggplot2::waiver(),
+  breaks = ggplot2::waiver(), labels = ggplot2::waiver(), limits = NULL, expand = ggplot2::waiver(),
+  na.translate = TRUE, na.value = NA, drop = TRUE,
+  guide = "legend", position = "left", super = ScaleHighlight,
+  predicate = NULL) {
+
+  position <- match.arg(position, c("left", "right", "top", "bottom"))
+
+  ggproto(NULL, super,
+          call = match.call(),
+
+          aesthetics = aesthetics,
+          scale_name = scale_name,
+          palette = palette,
+
+          range = ggplot2:::discrete_range(),
+          limits = limits,
+          na.value = na.value,
+          na.translate = na.translate,
+          expand = expand,
+
+          name = name,
+          breaks = breaks,
+          labels = labels,
+          drop = drop,
+          guide = guide,
+          position = position,
+          predicate = predicate
+  )
+}
