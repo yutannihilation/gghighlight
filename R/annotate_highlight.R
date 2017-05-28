e6945a53825516656b49ad034c14143aaf1dba37
Hiroaki Yutani <yutani.ini@gmail.com>
2017-05-28 15:38:21 +0900

    fix ungrouped annotate

--- R/annotate_highlight.R
+++ R/annotate_highlight.R
@@ -5,15 +5,27 @@ annotate_highlights <- function(.predicate, mapping, grouped = TRUE, ...) {
   predicate <- rlang::enquo(.predicate)
   key <- mapping$label
 
-  filter_func <- build_filter_func(predicate, key)
+  if(grouped) {
+    filter_func <- build_grouped_filter_func(predicate, key)
+  } else {
+    filter_func <- build_ungrouped_filter_func(predicate)
+  }
   ggrepel::geom_text_repel(mapping, data = filter_func, nudge_x = 45)
 }
 
-build_filter_func <- function(predicate, key = NULL) {
+build_grouped_filter_func <- function(predicate, key = NULL) {
   function(df) {
     gdf <- if(is.null(key)) df else dplyr::group_by(df, !! key)
     fdf <- dplyr::filter(gdf, !! predicate)
+    # TODO: provide a way to customize how to choose representions
     fdf <- dplyr::summarise_all(fdf, dplyr::last)
+    dplyr::ungroup(fdf)
+  }
+}
+
+build_ungrouped_filter_func <- function(predicate) {
+  function(df) {
+    fdf <- dplyr::filter(df, !! predicate)
     dplyr::ungroup(fdf)
   }
 }
