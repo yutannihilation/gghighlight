427e2e55f604dc777a360b40dc29ca5b725d6283
Hiroaki Yutani <yutani.ini@gmail.com>
2017-09-26 16:00:38 +0900

    update

--- R/geom_line.R
+++ R/geom_line.R
@@ -1,17 +1,20 @@
 #' Highlight Lines With Predicate
 #'
+#' @inheritDotParams ggplot2::geom_line
 #' @export
 geom_highlighted_line <- function(.predicate, mapping = NULL,
-                                  unhighlighted_colour = ggplot2::alpha("grey", 0.3), ...) {
+                                  unhighlighted_colour = ggplot2::alpha("grey", 0.3),
+                                  ...) {
   predicate <- rlang::enquo(.predicate)
-  key <- mapping$colour
 
-  if(is.null(key)) stop('Please specify colour aesthetics')
+  key <- if (is.null(mapping$group)) mapping$colour else mapping$group
+
+  if (is.null(key)) stop('Please specify colour or group aesthetics')
 
   filter_func <- build_grouped_filter_func(predicate, key)
 
   mapping_orig <- mapping
-  mapping$group <- mapping$group %||% mapping$colour
+  mapping$group <- key
 
   list(
     geom_line(mapping = mapping, colour = unhighlighted_colour),
