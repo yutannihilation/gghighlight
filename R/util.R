6e07970bc47cd78027b0ed57fc0fd37ae7de0bdd
Hiroaki Yutani <yutani.ini@gmail.com>
2017-09-29 07:47:50 +0900

    remove unused utils

--- R/util.R
+++ R/util.R
@@ -1,23 +1,5 @@
 # Utilities
 
-build_grouped_filter_func <- function(predicate, key) {
-  if (is.null(key)) {
-    return(build_ungrouped_filter_func(predicate))
-  }
-
-  function(df) {
-    df_grouped <- dplyr::group_by(df, !! key)
-    df_filtered <- dplyr::filter(df_grouped, !! predicate)
-    dplyr::ungroup(df_filtered)
-  }
-}
-
-build_ungrouped_filter_func <- function(predicate) {
-  function(df) {
-    dplyr::filter(df, !! predicate)
-  }
-}
-
 `%||%` <- function(lhs, rhs) {
   if (is.null(lhs)) {
     rhs
