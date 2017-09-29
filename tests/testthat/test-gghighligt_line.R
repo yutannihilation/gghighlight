81505cadd5231f25a873ecfc46a0e1934230241c
Hiroaki Yutani <yutani.ini@gmail.com>
2017-09-29 09:34:32 +0900

    add more test

--- tests/testthat/test-gghighligt_line.R
+++ tests/testthat/test-gghighligt_line.R
@@ -31,6 +31,26 @@ test_that("gghighligt_line() with usual arguments works", {
   expect_equal(length(unique(data_highlighted_label$group)), 2L)
 })
 
+test_that("gghighligt_line() without direct labeling works", {
+  expect_silent(p <- gghighlight_line(d, aes(idx, value, colour = category), max(value) > 10,
+                                      use_direct_label = FALSE))
+  d_built <- ggplot2::ggplot_build(p)
+
+  expect_equal(length(d_built$data), 2L)
+
+  data_unhighlighted <- d_built$data[[1]]
+  data_highlighted_line <- d_built$data[[2]]
+
+  # check if the unhighlighted data the same as the original one
+  expect_equal(nrow(data_unhighlighted), 9L)
+  expect_equal(length(unique(data_unhighlighted$group)), 3L)
+
+  # check if the highlited data is as expected
+  expect_equal(nrow(data_highlighted_line), 6L)
+  expect_equal(length(unique(data_highlighted_line$group)), 2L)
+})
+
+
 test_that("gghighligt_line() without colour mapping works", {
   expect_warning(p <- gghighlight_line(d, aes(idx, value), max(value) > 10))
   d_built <- ggplot2::ggplot_build(p)
@@ -49,3 +69,5 @@ test_that("gghighligt_line() without colour mapping works", {
   expect_equal(nrow(data_highlighted_line), 9L)
   expect_equal(length(unique(data_highlighted_line$group)), 1L)
 })
+
+
