9f46112aa6aa9274fd748a70f96ddb3615569a9a
Hiroaki Yutani <yutani.ini@gmail.com>
2020-03-28 18:23:27 +0900

    Explicitly sort the columns of data.frame

--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -1,6 +1,8 @@
 expect_equal_layer <- function(x, y) {
   x$mapping <- x$mapping[sort(names(x$mapping))]
   y$mapping <- y$mapping[sort(names(y$mapping))]
+  x$data <- x$data[, sort(colnames(x$data))]
+  y$data <- y$data[, sort(colnames(y$data))]
   expect_equal(x, y)
 }
 
