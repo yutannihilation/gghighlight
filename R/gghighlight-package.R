fd074f822925e71650b21070b2bfd7e24e8d318f
Hiroaki Yutani <yutani.ini@gmail.com>
2025-06-14 13:35:04 +0900

    Tweak for deprecation of %+%

--- R/gghighlight-package.R
+++ R/gghighlight-package.R
@@ -4,7 +4,6 @@
 #'
 #' @name gghighlight-package
 #' @import rlang
-#' @importFrom ggplot2 %+%
 #' @importFrom ggplot2 ggplot_add
 #' @importFrom dplyr desc across if_all all_of
 "_PACKAGE"
@@ -20,3 +19,12 @@ utils::globalVariables("where")
 #' @importFrom dplyr n
 #' @export
 dplyr::n
+
+# TODO: remove this when I remove support for ggplot2 v3
+`%+%` <- function(e1, e2) {
+  if (utils::packageVersion("ggplot2") > "3.5.2.9000") {
+    ggplot2::add_gg(e1, e2)
+  } else {
+    ggplot2::`%::%`(e1, e2)
+  }
+}
